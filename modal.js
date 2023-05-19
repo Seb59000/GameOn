3aa9f02d1d56f90a19cd40087b3000bd339aaf2c
Seb59000 <36163352+Seb59000@users.noreply.github.com>
2023-05-19 16:05:24 +0200

    message confirmation OK

--- modal.js
+++ modal.js
@@ -8,20 +8,61 @@ function editNav() {
 }
 
 // DOM Elements
-const modalbg = document.querySelector(".bground");
+const modal = document.querySelector(".modal");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
-const closeModal = document.querySelector(".close");
+const closeModal = document.querySelector(".close-form");
+
+const confirmation = document.querySelector(".confirmation");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
 // launch modal form
 function launchModal() {
-  modalbg.style.display = "block";
+  modal.style.display = "block";
 }
 
-// close modal form
+// button close modal form
 closeModal.addEventListener("click", () => {
-  modalbg.style.display = "none";
+  modal.style.display = "none";
 });
+
+// verif si form a été validé
+verifFormValidated();
+
+function verifFormValidated() {
+  const valid = sessionStorage.getItem('valid');
+
+  // si form validé
+  if (valid != null) {
+    // Suppr des données de sessionStorage
+    sessionStorage.removeItem('valid');
+    // affichage message confirmation
+    showConfirmationInscription();
+  }
+}
+
+// validation form
+function Validate() {
+  let valid = true;
+
+  // si tout est OK
+  if (valid) {
+    // affichage modale confirmation inscription
+    sessionStorage.setItem('valid', '1');
+    return true;
+  } else {
+    return false;
+  }
+}
+
+// display confimration dialog
+function showConfirmationInscription() {
+  confirmation.style.display = "block";
+}
+
+// close confimration dialog
+function closeConfirmationInscription() {
+  confirmation.style.display = "none";
+}
